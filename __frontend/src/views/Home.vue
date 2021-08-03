aeb334ddd8444c33f910b11b4c73cf131a3abe13
Raphael <veauville.raphael@gmail.com>
2021-08-03 21:42:52 +0200

    Update

--- __frontend/src/views/Home.vue
+++ __frontend/src/views/Home.vue
@@ -5,7 +5,7 @@
       <h1 id="smaug">Smaug website</h1>
       <p>
         <a href="https://discord.com/users/611468402263064577" target="__blank">
-          <Button color="#545aa7" size="large">
+          <Button color="#7278c2" size="large">
             <img
               src="@/assets/discord.svg"
               alt="discord"
@@ -16,7 +16,7 @@
           </Button>
         </a>
         <a href="https://github.com/Smaug6739" target="__blank">
-          <Button color="#545aa7" size="large">
+          <Button color="#7278c2" size="large">
             <img
               src="@/assets/github.svg"
               alt="github"
@@ -27,7 +27,7 @@
           </Button>
         </a>
         <a href="mailto:raphael.veauville@gmail.com" target="__blank">
-          <Button color="#545aa7" size="large">
+          <Button color="#7278c2" size="large">
             <img
               src="@/assets/email.svg"
               alt="github"
@@ -40,35 +40,74 @@
       </p>
       <p>
         <img
-          src="https://img.shields.io/static/v1?label=Uses&message=HTML&color=green"
+          src="https://img.shields.io/badge/Uses-HTML-informational?style=flat&logo=Uses&logoColor=white&color=2bbc8a"
+          
+
           alt="HTML"
           class="badge"
         />
         <img
-          src="https://img.shields.io/static/v1?label=Uses&message=CSS&color=green"
+          src="https://img.shields.io/badge/Uses-CSS-informational?style=flat&logo=Uses&logoColor=white&color=2bbc8a"
           alt="CSS"
           class="badge"
         />
         <img
-          src="https://img.shields.io/static/v1?label=Uses&message=Javascript&color=green"
+          src="https://img.shields.io/badge/Uses-Javascript-informational?style=flat&logo=Uses&logoColor=white&color=2bbc8a"
           alt="Javascript"
           class="badge"
         />
         <img
-          src="https://img.shields.io/static/v1?label=Uses&message=Typescript&color=green"
+          src="https://img.shields.io/badge/Uses-Typescript-informational?style=flat&logo=Uses&logoColor=white&color=2bbc8a"
           alt="Typescript"
           class="badge"
         />
         <img
-          src="https://img.shields.io/static/v1?label=Uses&message=Node.js&color=green"
+          src="https://img.shields.io/badge/Uses-Node.js-informational?style=flat&logo=Uses&logoColor=white&color=2bbc8a"
           alt="Node.js"
           class="badge"
         />
         <img
-          src="https://img.shields.io/static/v1?label=Uses&message=PHP&color=green"
+          src="https://img.shields.io/badge/Uses-PHP-informational?style=flat&logo=Uses&logoColor=white&color=2bbc8a"
           alt="PHP"
           class="badge"
         />
+        <img
+          src="https://img.shields.io/badge/Uses-C++-informational?style=flat&logo=Uses&logoColor=white&color=2bbc8a"
+          alt="C++"
+          class="badge"
+        />
+      </p>
+      <p>
+        <img
+          src="https://img.shields.io/badge/Uses-MongoDB-informational?style=flat&logo=Uses&logoColor=white&color=2bbc8a"
+          
+
+          alt="MongoDB"
+          class="badge"
+        />
+        <img
+          src="https://img.shields.io/badge/Uses-MySQL-informational?style=flat&logo=Uses&logoColor=white&color=2bbc8a"
+          alt="MySQL"
+          class="badge"
+        />
+        <img
+          src="https://img.shields.io/badge/Uses-Postgres-informational?style=flat&logo=Uses&logoColor=white&color=2bbc8a"
+          alt="Postgres"
+          class="badge"
+        />
+        <img
+          src="https://img.shields.io/badge/Uses-Git-informational?style=flat&logo=Uses&logoColor=white&color=2bbc8a"
+          
+
+          alt="Git"
+          class="badge"
+        />
+        <img
+          src="https://img.shields.io/badge/Uses-Docker-informational?style=flat&logo=Uses&logoColor=white&color=2bbc8a"
+          alt="Docker"
+          class="badge"
+        />
+
       </p>
     </div>
     <div id="projects">
@@ -95,7 +134,7 @@ export default {
 <style scoped lang='scss'>
 @import "../../public/scss/theme-variables";
 #home {
-  background-image: url("../assets/background.webp");
+  background-image: url("../assets/header-test.jpg");
   background-repeat: no-repeat;
   height: 40vh;
   #smaug {
